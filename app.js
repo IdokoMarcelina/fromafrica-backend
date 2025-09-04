587cc468a1e6a46de7a6f654e3d71a5c05ed8c24
IdokoMarcelina <aitmacelina@gmail.com>
2025-09-04 10:56:28 +0100

    fix2

--- app.js
+++ app.js
@@ -110,6 +110,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:5175',
+  'http://localhost:5176',
+  'http://localhost:5177',
   'http://admin-africa.vercel.app',
   'https://admin-eight-sigma-88.vercel.app'
 ];
